4504dc8e0a297cde7cc7a37ec99ee78a19f11292
Stepanov Artem <artemis6218@yandex.ru>
2021-11-10 08:02:02 +0300

    -added detail page

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 def all_blogs(request):
     blogs = Post.objects.all()
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/detail.html', {'post':post})
